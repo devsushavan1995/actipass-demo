98b289de0a863522969b3ab151eb532d6a9a53a6
devsushavan1995 <devsushavan95@gmail.com>
2021-10-20 16:15:00 +0530

    dashboard two tabs added for two child

--- js/script.js
+++ js/script.js
@@ -471,6 +471,7 @@ $(document).ready(function () {
 
   // Chart js for child progress
   var ctx = $('#childName1Progress');
+  var ctx2 = $('#childName2Progress');
 var childName1Progress = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -504,5 +505,39 @@ var childName1Progress = new Chart(ctx, {
             }
         }
     }
+});
+var childName1Progress = new Chart(ctx2, {
+  type: 'bar',
+  data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          y: {
+              beginAtZero: true
+          }
+      }
+  }
 });
 });
