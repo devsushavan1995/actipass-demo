df779022f8ca769a370fd236e5f1caf78e10aa79
devsushavan1995 <devsushavan95@gmail.com>
2021-10-22 17:39:29 +0530

    browse page mobile version add

--- js/script.js
+++ js/script.js
@@ -478,7 +478,38 @@ $(document).ready(function () {
     //   //then show the tab content of whatever option value was selected
     //   $('#' + 'tab-' + dropdown).show();
     // });
+    // add story custom file button
+    $('input[type="file"]').each(function() {
+      // get label text
+      var label = $(this).parents('.form-group').find('label').text();
+      label = (label) ? label : 'Upload File';
   
+      // wrap the file input
+      $(this).wrap('<div class="input-file"></div>');
+      // display label
+      $(this).before('<span class="btn"><span class="bi bi-upload ml-1"></span></span>');
+      // we will display selected file here
+      $(this).before('<span class="file-selected"></span>');
+  
+      // file input change listener 
+      $(this).change(function(e){
+          // Get this file input value
+          var val = $(this).val();
+          
+          // Let's only show filename.
+          // By default file input value is a fullpath, something like 
+          // C:\fakepath\Nuriootpa1.jpg depending on your browser.
+          var filename = val.replace(/^.*[\\\/]/, '');
+  
+          // Display the filename
+          $(this).siblings('.file-selected').text(filename);
+      });
+  });
+  
+  // Open the file browser when our custom button is clicked.
+  $('.input-file .btn').click(function() {
+      $(this).siblings('input[type="file"]').trigger('click');
+  });
     // Chart js for child progress
     var ctx = $('#childName1Progress');
     var ctx2 = $('#childName2Progress');
